82907d327b32bf8cf1d2b1cb693691ae4ca8c6b1
Andy Eblin <aeblin@gmail.com>
2014-05-13 04:22:43 -0700

    Styling changes to index.html.erb, addition of EG favicon.

--- config/initializers/launch_soon.rb
+++ config/initializers/launch_soon.rb
@@ -5,22 +5,22 @@ MAILCHIMP_API_KEY = ENV['MAILCHIMP_API_KEY']
 MAILCHIMP_LIST_ID = ENV['MAILCHIMP_LIST_ID']
 
 # This site's domain name, including the www. if required (ie: google.com):
-SITE_DOMAIN = 'DOMAIN.TLD'
+SITE_DOMAIN = 'overworkedandoverwhelmed.com'
 
 # Title tag for the site:
 SITE_TITLE = 'Overworked and Overwhelmed'
 
 # Keywords for the site:
-SITE_KEYWORDS = 'key, words'
+SITE_KEYWORDS = 'Scott, Eblin, Eblin Group, Overworked, Overwhelmed, Exhausted, Book, Next Level, Executive, Coaching, Corporate, Vibrant, Zen'
 
-# Description for the site, also used in Tweet so keep it short:
-SITE_DESCRIPTION = 'DESCRIPTION'
+# # Description for the site, also used in Tweet so keep it short:
+SITE_DESCRIPTION = 'Overworked and Overwhelmed - by Scott Eblin'
 
 # Blurb about site, displayed on main page:
-SITE_BLURB = 'Words, words, words.'
+# SITE_BLURB = 'Words, words, words.'
 
 # Google Analytics code for the site:
 SITE_GOOGLE_ANALYTICS = 'UA-#######-##'
 
 # Twitter name for site:
-TWITTER_NAME = 'TWITTERUSERNAME'
+TWITTER_NAME = 'ScottEblin'
